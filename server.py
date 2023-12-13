057112c3f8c15772f7c8eb01eafaaccf5bad0a58
herilala <hei.herilala@gmail.com>
2023-12-13 09:50:57 +0300

    feat: add all exeption in get exept RequestTimeout exeption

--- server.py
+++ server.py
@@ -1,10 +1,11 @@
-import asyncio
+import schedule
 import time
 from datetime import datetime
 from fastapi import FastAPI, File, HTTPException, Query, UploadFile
 from fastapi.responses import JSONResponse
 from fastapi.responses import FileResponse
 import logging
+import threading
 import os
 
 log_format = "%(asctime)s - %(levelname)s - %(pathname)s:%(lineno)d - %(message)s"
@@ -14,11 +15,25 @@ app = FastAPI()
 
 separation_message_type = ": "
 
-error_status_codes = [400, 402, 403]
+request_count = 0
+
+error_status_codes = [400, 402, 403, 501]
+
+not_legal_image_name = ["not_legal.jpg", "forbidden_document.jpg", "pirate.jpg"]
+
+
+def reset_request_count():
+    global request_count
+    request_count = 0
 
 
 def cast_message_to_exception_message(error_type, message):
     return f"{error_type}{separation_message_type}{message}"
+
+
+schedule.every(1).seconds.do(reset_request_count)
+
+# thread = threading.Timer(1, lambda: globals().update(raise_exception(408, "RequestTimeout", f"message")))
 
 
 def raise_exception(code, tyme, message):
@@ -64,29 +79,41 @@ for status_code in error_status_codes:
         )
 
 
+@app.get("/delete")
+def delete_file():
+    try:
+        raise raise_exception(501, "NotImplemented", f"the endpoint /delete is not implemented ")
+    except HTTPException as e:
+        logging.error(e.detail)
+        raise e
+
+
 # Endpoint that checks the value of the query parameter "n"
 @app.get("/picture")
 async def get_picture(
         file_name: str = Query(..., description="Name of the image file"),
 ):
     try:
+        # thread.start()
+        schedule.run_pending()
+        global request_count
+        request_count += 1
         file_path = os.path.join("picture", file_name)
+
+        if request_count > 2:
+            raise raise_exception(429, "TooManyRequest", f"must not execute request more than 2 times per second")
+
         if not os.path.exists(file_path):
-            raise raise_exception(404, "FileNotFound", f"{file_name} is not found.")
+            raise raise_exception(404, "FileNotFound", f"the file with name '{file_name}' is not found.")
 
-        time.sleep(60)
-
-        if 1 == 2:
-            raise raise_exception(501, "NotImplemented", f"message")
-        if 1 == 2:
-            raise raise_exception(453, "LegalReason", f"message")
-        if 1 == 2:
-            raise raise_exception(429, "TooManyRequest", f"message")
+        if file_name in not_legal_image_name:
+            raise raise_exception(453, "LegalReason",
+                                  f"the file with name '{file_name}' cannot be downloaded for legal reasons")
 
         return FileResponse(file_path, media_type="image/jpeg",
                             headers={"Content-Disposition": f"filename={file_name}"})
     except TimeoutError as e:
-        logging.error(exc.detail)
+        logging.error(e.detail)
         raise raise_exception(408, "RequestTimeout", f"message")
     except Exception as e:
         logging.error(e.detail)
@@ -148,6 +175,8 @@ async def upload_files(files: list[UploadFile] = File(...)):
     except Exception as e:
         logging.error(e.detail)
         raise e
+
+
 if __name__ == "__main__":
     import uvicorn
 
